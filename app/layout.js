c0b7f09eb4ff579f29d92250be5e42eccbbacff8
Abdulmoizk <136237967+Abdulmoizk@users.noreply.github.com>
2024-03-19 02:00:31 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,6 @@ import React, { useEffect } from "react";
 import { Cardo, Nunito } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
-import { trace } from "next/dist/trace";
 import AOS from "aos";
 import dynamic from "next/dynamic";
 const PureCounter = dynamic(() => import("@srexi/purecounterjs"), {
